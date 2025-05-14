24a3dfc5e7035b7a9b196ab1541ece90ee877738
Anthonybm8 <anthony.monta2291@utc.edu.ec>
2025-05-14 17:08:17 -0500

    Se agregaron las aplicaciones

--- sw_coacmlp/settings.py
+++ sw_coacmlp/settings.py
@@ -43,7 +43,11 @@ INSTALLED_APPS = [
     'Aplicaciones.Historia',
     'Aplicaciones.Noticia',
     'Aplicaciones.Objetivos',
+    'Aplicaciones.Pregunta',
+    'Aplicaciones.Progreso',
 
+
+    
 ]
 
 MIDDLEWARE = [
